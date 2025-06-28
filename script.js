c65a6f58a8894829006fada1785c076ce52f8116
Abhay Katiyar <www.abhaykatiyar@gmail.com>
2025-06-28 20:12:33 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+ScrollReveal().reveal('.main-left', { origin: 'left', distance: '50px', duration: 1000, delay: 300 });
+ScrollReveal().reveal('.main-right', { origin: 'right', distance: '50px', duration: 1000, delay: 500 });
+ScrollReveal().reveal('.navbar', { origin: 'top', distance: '20px', duration: 800, delay: 200 });
+
+ScrollReveal().reveal('.about-me-left', {
+  origin: 'left',
+  distance: '50px',
+  duration: 1000,
+  delay: 200
+});
+ScrollReveal().reveal('.about-me', {
+  origin: 'right',
+  distance: '50px',
+  duration: 1000,
+  delay: 300
+});
+
+
+ScrollReveal().reveal('.skills-section', {
+  origin: 'bottom',
+  distance: '60px',
+  duration: 1000,
+  delay: 300,
+});
